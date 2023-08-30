44a9e92b93c2fce4c7ec53fbe752081c22efa5a9
Aviad <134489612+AviadLange@users.noreply.github.com>
2023-08-30 14:48:40 +0300

    Delete Points+Franchise.sql

--- Points+Franchise.sql
+++
@@ -1,6 +0,0 @@
---This query joins the Points Data to Franchise Division.
-SELECT *
-FROM "Points Data" AS p
-JOIN "Franchise Division" AS f
-ON f.team_id = p.team_id
-AND p.team_name = f.city || f.franchise;
